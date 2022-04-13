78e6fa889035420385a176ea030e273811ad1ae4
Andrew Wells <56801781+awells-45@users.noreply.github.com>
2022-04-13 13:46:10 -0700

    Add files via upload

---
+++ NeonBreakout/Assets/Demos/Paddle and Input Demo/UserInput.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UserInput : MonoBehaviour
+{
+    public UnityEvent OnUpInput;
+    public UnityEvent OnDownInput;
+    public UnityEvent OnLeftInput;
+    public UnityEvent OnRightInput;
+    public UnityEvent OnEscapeInput;
+    public UnityEvent OnEnterInput;
+    public UnityEvent OnSpaceInput;
+
+    private void Update()
+    {
+        GetUserInput();
+    }
+    
+    private void GetUserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            OnSpaceInput.Invoke();
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            OnEnterInput.Invoke();
+        if (Input.GetKeyDown(KeyCode.Escape))
+            OnEscapeInput.Invoke();
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+            OnUpInput.Invoke();
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+            OnDownInput.Invoke();
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            OnLeftInput.Invoke();
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            OnRightInput.Invoke();
+    }
+}
