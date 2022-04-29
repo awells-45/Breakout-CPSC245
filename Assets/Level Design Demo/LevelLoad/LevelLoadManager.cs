39a8e15125dbd9ee4a876c754deec8fc058000fd
Lucas Torti <47060346+LucTort@users.noreply.github.com>
2022-04-29 10:03:02 -0700

    Add Unity event "OnLevelComplete", make LevelLoadManager a singleton

--- Assets/Level Design Demo/LevelLoad/LevelLoadManager.cs
+++ Assets/Level Design Demo/LevelLoad/LevelLoadManager.cs
@@ -20,12 +20,21 @@ using UnityEngine.Audio;
 public class LevelLoadManager : MonoBehaviour
 {
     //https://www.youtube.com/watch?v=CPKAgyp8cno&t=660s
+    public static LevelLoadManager instance;
+
+
+    //Create Unity event
+    public delegate void LevelCompleted();
+    public static event LevelCompleted OnLevelCompleted;
+    // In order to use this, place the following code in your script's Awake/Start method:
+    // LevelCompleted.OnLevelCompleted += yourFunctionHere;
+    // Once above code is added, "yourFunctionHere" will be called every time the "OnLevelCompleted" event occurs
 
     public int activeBricks = 0;
     public Level[] levels;
     public int currentLevel = 0;
     public ObjectPool objectPool;
-    
+
     // Loads the current level, then adds 1 to currentLevel to load the next level next time
     public void LoadLevel()
     {
@@ -40,7 +49,7 @@ public class LevelLoadManager : MonoBehaviour
         objectPool.PlaceBricks(getBrickLocations(level), getBrickSprites(level));
         activeBricks = level.bricks.Count;
     }
-    
+
     // Decrements the amount of active bricks (presumably when a brick gets hit)
     // Writes to the log when the amount of active bricks has hit 0
     public void DecrementActiveBricks()
@@ -51,6 +60,7 @@ public class LevelLoadManager : MonoBehaviour
             // NOTE: This is a temporary solution. We put this here because we do not know
             // how the game manager team wants level completion implemented, though this provides a base.
             Debug.Log("There are no more bricks!");
+            OnLevelCompleted();
         }
     }
 
@@ -61,7 +71,7 @@ public class LevelLoadManager : MonoBehaviour
         currentLevel = 0;
         LoadLevel();
     }
-    
+
     // Goes through each brick in the level and creates a list of their positions, then returns that list
     // Used in level loading to assign locations to object-pooled bricks
     private List<Vector2> getBrickLocations(Level nextLevel)
@@ -74,7 +84,7 @@ public class LevelLoadManager : MonoBehaviour
 
         return brickPositions;
     }
-    
+
     // Goes through each brick in the level and creates a list of their sprites, then returns that list
     // Used in level loading to assign sprites to object-pooled bricks
     private List<Sprite> getBrickSprites(Level nextLevel)
