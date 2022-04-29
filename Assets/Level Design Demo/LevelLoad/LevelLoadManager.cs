f38de1a658857d27b966531676919531339aa0fa
IanR8612 <ireafs@protonmail.com>
2022-04-29 09:46:45 -0700

    Added comments to LevelLoadManager and removed outdated methods

--- Assets/Level Design Demo/LevelLoad/LevelLoadManager.cs
+++ Assets/Level Design Demo/LevelLoad/LevelLoadManager.cs
@@ -1,4 +1,14 @@
-﻿//Created by Ian Reafsnyder and Lucas Torti
+﻿/* a. Ian Reafsnyder, Lucas Torti, and Natalie Huante
+ * b. 2337621, 2351555, 2374481
+ * c. ireafsnyder@chapman.edu, torti@chapman.edu, huante@chapman.edu
+ * d. CPSC 245-01
+ * e. Brick buster class project
+ *
+ * Manages the loading and resetting of levels in the game.
+ * Contains an array containing all levels in the game.
+ */
+
+//Created by Ian Reafsnyder and Lucas Torti
 //Overall purpose: Contains a list of levels and loads the levels when the previous level has been completed.
 
 using System;
@@ -15,18 +25,8 @@ public class LevelLoadManager : MonoBehaviour
     public Level[] levels;
     public int currentLevel = 0;
     public ObjectPool objectPool;
-
-    private void Awake()
-    {
-        // objectPool = ObjectPool.sharedInstance;
-    }
-
-    public void Start()
-    {
-        // This is for testing, in reality LoadLevel should be called by the game manager
-        //LoadLevel();
-    }
-
+    
+    // Loads the current level, then adds 1 to currentLevel to load the next level next time
     public void LoadLevel()
     {
         Debug.Log(currentLevel);
@@ -40,22 +40,30 @@ public class LevelLoadManager : MonoBehaviour
         objectPool.PlaceBricks(getBrickLocations(level), getBrickSprites(level));
         activeBricks = level.bricks.Count;
     }
-
-    public static void AddBrick(BlueprintBrick brick)
-    {
-        // use the function contained in the bricks script to place the bricks
-        Debug.Log(brick.brickTransform + " " + brick.brickSprite);
-    }
-
+    
+    // Decrements the amount of active bricks (presumably when a brick gets hit)
+    // Writes to the log when the amount of active bricks has hit 0
     public void DecrementActiveBricks()
     {
         activeBricks -= 1;
         if (activeBricks == 0)
         {
-            LoadLevel();
+            // NOTE: This is a temporary solution. We put this here because we do not know
+            // how the game manager team wants level completion implemented, though this provides a base.
+            Debug.Log("There are no more bricks!");
         }
     }
 
+    // Sets all bricks to inactive, resets the current level to 0, and loads level 0
+    public void Reset()
+    {
+        objectPool.Reset();
+        currentLevel = 0;
+        LoadLevel();
+    }
+    
+    // Goes through each brick in the level and creates a list of their positions, then returns that list
+    // Used in level loading to assign locations to object-pooled bricks
     private List<Vector2> getBrickLocations(Level nextLevel)
     {
         List<Vector2> brickPositions = new List<Vector2>();
@@ -66,7 +74,9 @@ public class LevelLoadManager : MonoBehaviour
 
         return brickPositions;
     }
-
+    
+    // Goes through each brick in the level and creates a list of their sprites, then returns that list
+    // Used in level loading to assign sprites to object-pooled bricks
     private List<Sprite> getBrickSprites(Level nextLevel)
     {
         List<Sprite> brickSprites = new List<Sprite>();
