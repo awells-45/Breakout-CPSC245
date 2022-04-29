8561d3dec1a69d68b9e7e7429390de9c23f63ba3
Lucas Torti <47060346+LucTort@users.noreply.github.com>
2022-04-29 10:19:12 -0700

    Add comments to ObjectPooling

--- Assets/Level Design Demo/ObjectPooling Demo/Scripts/ObjectPool.cs
+++ Assets/Level Design Demo/ObjectPooling Demo/Scripts/ObjectPool.cs
@@ -1,3 +1,16 @@
+/* a. Ian Reafsnyder, Lucas Torti, and Natalie Huante
+ * b. 2337621, 2351555, 2374481
+ * c. ireafsnyder@chapman.edu, torti@chapman.edu, huante@chapman.edu
+ * d. CPSC 245-01
+ * e. Brick buster class project
+ *
+ * Manages the loading and resetting of levels in the game.
+ * Contains an array containing all levels in the game.
+ */
+
+//Created by Natalie Huante
+//Overall purpose: Maintains and manages a pool of brick objects for increased computational efficiency
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -14,9 +27,10 @@ public class ObjectPool : MonoBehaviour
     public int amountActive;
 
 
+    // creates a singleton named sharedInstance
     private void Awake()
     {
-        sharedInstance = this; 
+        sharedInstance = this;
     }
 
     private void Start()
@@ -29,10 +43,11 @@ public class ObjectPool : MonoBehaviour
             temp = Instantiate(objectToPool); // instantiate a new brick
             temp.gameObject.SetActive(false); // set it to inactive
             pooledBricks.Add(temp); // add to the object pool 
-            print("made brick: " + (i+1));
+            print("made brick: " + (i + 1));
         }
     }
 
+    // sets all the brick objects to be inactive, and updates the relevant amountActive value to be 0
     public void Reset()
     {
         // resets all bricks to inactive
@@ -44,6 +59,7 @@ public class ObjectPool : MonoBehaviour
         amountActive = 0;
     }
 
+    // return the next available brick which is not currently in use
     private Brick GetPooledObject()
     {
         for (int i = 0; i < amountToPool; ++i)
@@ -56,6 +72,8 @@ public class ObjectPool : MonoBehaviour
         return null;
     }
 
+    // given a list of positions and images, places the bricks accordingly and increments 
+    //amountActive by the number of placed bricks
     public void PlaceBricks(List<Vector2> positions, List<Sprite> images)
     {
         print("placing level bricks");
@@ -65,6 +83,6 @@ public class ObjectPool : MonoBehaviour
             objectToPlace.SetBrick(positions[i], images[i]);
             amountActive++;
         }
-        
+
     }
 }
