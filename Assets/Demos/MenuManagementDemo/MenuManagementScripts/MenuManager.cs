9e35b7429ede46b90abe695117e2fa51cda9f8c3
Audrey Takenaga <69403863+takenaga318@users.noreply.github.com>
2022-05-02 12:36:52 -0700

    Updated functionality of MenuManager with TestButtons invoking events

--- Assets/Demos/MenuManagementDemo/MenuManagementScripts/MenuManager.cs
+++ Assets/Demos/MenuManagementDemo/MenuManagementScripts/MenuManager.cs
@@ -1,57 +1,132 @@
-using UnityEditor.U2D;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-	private bool isPaused = false;
-    private void OnEnable()
+	//private bool isPaused = false;
+	[SerializeField] private CanvasGroup mainMenuGroup;
+	[SerializeField] private CanvasGroup pauseMenuGroup;
+	[SerializeField] private CanvasGroup loseScreenGroup;
+	[SerializeField] private CanvasGroup winScreenGroup;
+
+	private void OnEnable()
     {
-	    // GameStateWon.WonStateBegin += ShowWinScreen();
-	    // GameStateWon.WonStateEnd += HideWinScreen();
-	    // GameStateLost.LoseStateBegin += ShowLoseScreen();
-	    // GameStateWon.WonStateEnd += HideLoseScreen();
-	    //TestButtons.MainMenu += 
+	    //Actual GameState Events MenuManager should subscribe to
+	    // GameStateWon.WonStateBegin += ShowWinScreen;
+	    // GameStateWon.WonStateEnd += HideWinScreen;
+	    // GameStateLost.LoseStateBegin += ShowLoseScreen;
+	    // GameStateWon.WonStateEnd += HideLoseScreen;
+	    // GameStatePaused.PauseStateBegin += ShowPauseMenu;
+	    // GameStatePaused.PauseStateEnd += HidePauseMenu;
+	    // GameStateMainMenu.MainMenuStateBegin += ShowMainMenu;
+	    // GameStateMainMenu.MainMenuStateEnd += HideMainMenu;
+	    
+	    //subscribing the Show and Pause methods to the test events on TestButtons
+	    TestButtons.PauseMenuStart += ShowPauseMenu;
+	    TestButtons.PauseMenuEnd += HidePauseMenu;
+	    TestButtons.MainMenuStart += ShowMainMenu;
+	    TestButtons.MainMenuEnd += HideMainMenu;
+	    TestButtons.WinScreenStart += ShowWinScreen;
+	    TestButtons.WinScreenEnd += HideWinScreen;
+	    TestButtons.LoseScreenStart += ShowLoseScreen;
+	    TestButtons.LoseScreenEnd += HideLoseScreen;
     }
     
-	private void Update() { 
-		if (Input.GetKeyDown(KeyCode.Escape)) {
-			isPaused = !isPaused;
-			if(isPaused)
-			{ 
-				//GameStatePaused.PauseStateBegin += ShowPauseMenu();
-			}
-			else
-			{ 
-				//GameStatePaused.PauseStateEnd += HidePauseMenu();
-			}
-		}
-	}
+	//Input that I'm not sure which script is handling
+	// private void Update() { 
+	// 	if (Input.GetKeyDown(KeyCode.Escape)) {
+	// 		isPaused = !isPaused;
+	// 		if(isPaused)
+	// 		{ 
+	// 			gameManager.ChangeToNewState(GameStatePaused);
+	// 		}
+	// 		else
+	// 		{ 
+	// 			gameManager.ChangeToNewState(GameStatePlaying);
+	// 		}
+	// 	}
+	// }
     
-    	public void OnPauseButtonClick()
-        {
-	        isPaused = !isPaused;
-	        if(isPaused)
-	        { 
-		        //GameStatePaused.PauseStateBegin += ShowPauseMenu();
-	        }
-	        else
-	        { 
-		        //GameStatePaused.PauseStateEnd += HidePauseMenu();
-	        }
-        }
+	// public void OnPauseButtonClick()
+	// {
+	// 	isPaused = !isPaused;
+	// 	if(isPaused)
+	// 	{ 
+	// 		gameManager.ChangeToNewState(GameStatePaused);
+	// 	}
+	// 	else
+	// 	{ 
+	// 		gameManager.ChangeToNewState(GameStatePlaying);
+	// 	}
+	// }
     
-    	public void OnMainMenuButtonClick()
-        {
-	        //GameStateMainMenu.MainMenuStateBegin += ShowMainMenu();
-        }
-    	public void OnPlayButtonClick()
-        {
-	        //GameStatePlaying.PlayingStateBegin += ShowPlayScreen();
-        }
+	// public void OnMainMenuButtonClick()
+	// {
+	// 	gameManager.ChangeToNewState(GameStateMainMenu);
+	// }
+	// public void OnPlayButtonClick()
+	// {
+	// 	gameManager.ChangeToNewState(GameStatePlaying);
+	// }
         
-    	public void OnQuitButtonClick()
-        {
-	        Application.Quit();
-        }
+	public void OnQuitButtonClick()
+	{
+		Application.Quit();
+	}
+
+	private void ShowMainMenu()
+	{
+		Show(mainMenuGroup);
+	}
+
+	private void HideMainMenu()
+	{
+		Hide(mainMenuGroup);
+	}
+	
+	private void ShowPauseMenu()
+	{
+		Show(pauseMenuGroup);
+	}
+
+	private void HidePauseMenu()
+	{
+		Hide(pauseMenuGroup);
+	}
+	
+	private void ShowLoseScreen()
+	{
+		Show(loseScreenGroup);
+	}
+
+	private void HideLoseScreen()
+	{
+		Hide(loseScreenGroup);
+	}
+	
+	private void ShowWinScreen()
+	{
+		Show(winScreenGroup);
+	}
+	
+	private void HideWinScreen()
+	{
+		Hide(winScreenGroup);
+	}
+	
+	//Hiding the screen invisible and disabling interaction
+	private void Hide(CanvasGroup group)
+	{
+		group.alpha = 0; 
+		group.interactable = false; 
+		group.blocksRaycasts = false;
+	}
+	
+	//Showing the screen and enabling interaction
+	private void Show(CanvasGroup group)
+	{
+		group.alpha = 1;
+		group.interactable = true;
+		group.blocksRaycasts = true;
+	}
 
 }
--- Assets/Demos/MenuManagementDemo/MenuManagementScripts/TestButtons.cs
+++ Assets/Demos/MenuManagementDemo/MenuManagementScripts/TestButtons.cs
@@ -1,24 +1,77 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TestButtons : MonoBehaviour
 {
-
+    //Events mimicking the GameState GameStateEnter and GameStateExit methods on each GameState class
     public delegate void PublishState();
 
-    public static event PublishState MainMenu;
-    public static event PublishState PauseMenu;
-    public static event PublishState PlayScreen;
-    public static event PublishState WinScreen;
-    public static event PublishState LoseScreen;
-
-    public void OnClick(PublishState newState)
+    public static event PublishState MainMenuStart;
+    public static event PublishState MainMenuEnd;
+    public static event PublishState PauseMenuStart;
+    public static event PublishState PauseMenuEnd;
+    public static event PublishState WinScreenStart;
+    public static event PublishState WinScreenEnd;
+    public static event PublishState LoseScreenStart;
+    public static event PublishState LoseScreenEnd;
+    
+    //All the methods mimic the OnStateEnter() and OnStateExit() methods that invoke the events in each GameState class
+    public void OnClickPlay()  //Play screen is the layer under all the canvas group
+        //To play just hide the main menu
     {
-        if (newState != null)
+        if (MainMenuEnd != null)
         {
-            newState();
+            MainMenuEnd();
+        }
+    }
+    
+    public void OnClickPause()
+    {
+        if (PauseMenuStart != null)
+        {
+            PauseMenuStart();
+        }
+    }
+
+    public void OnClickResume()
+    {
+        if (PauseMenuEnd != null)
+        {
+            PauseMenuEnd();
+        }
+    }
+    
+    public void OnClickMainMenu()
+    {
+        if (MainMenuStart != null)
+        {
+            MainMenuStart();
+        }
+    }
+
+    public void OnClickReturn()
+    {
+        if (MainMenuStart != null && LoseScreenEnd != null && WinScreenEnd != null && PauseMenuEnd != null)
+        {
+            LoseScreenEnd();
+            WinScreenEnd();
+            PauseMenuEnd();
+            MainMenuStart();
+        }
+    }
+    
+    public void OnClickLose()
+    {
+        if (LoseScreenStart != null)
+        {
+            LoseScreenStart();
+        }
+    }
+    
+    public void OnClickWin()
+    {
+        if (WinScreenStart != null)
+        {
+           WinScreenStart();
         }
     }
 }
