5c6930f0dd03d4014366bf7d81eca43055b43541
Lucas Torti <47060346+LucTort@users.noreply.github.com>
2022-05-04 14:15:55 -0700

    Create new Gameplay scene and start merge

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -3,131 +3,131 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-	//private bool isPaused = false;
-	[SerializeField] private CanvasGroup mainMenuGroup;
-	[SerializeField] private CanvasGroup pauseMenuGroup;
-	[SerializeField] private CanvasGroup loseScreenGroup;
-	[SerializeField] private CanvasGroup winScreenGroup;
-	[SerializeField] private CanvasGroup playingScreenGroup;
+    //private bool isPaused = false;
+    [SerializeField] private CanvasGroup mainMenuGroup;
+    [SerializeField] private CanvasGroup pauseMenuGroup;
+    [SerializeField] private CanvasGroup loseScreenGroup;
+    [SerializeField] private CanvasGroup winScreenGroup;
+    [SerializeField] private CanvasGroup playingScreenGroup;
 
-	private void OnEnable()
+    private void OnEnable()
     {
-	     //GameState Events MenuManager should subscribe to
-	     //Hiding the Win and Lose is 
-	     GameStateWon.WonStateBegin += ShowWinScreen;
-	     //GameStateWon.WonStateEnd += HideWinScreen;
-	     GameStateLost.LoseStateBegin += ShowLoseScreen;
-	     //GameStateWon.WonStateEnd += HideLoseScreen;
-	     GameStatePaused.PauseStateBegin += ShowPauseMenu;
-	     GameStatePaused.PauseStateEnd += HidePauseMenu;
-	     GameStateMainMenu.MainMenuStateBegin += ShowMainMenu;
-	     GameStateMainMenu.MainMenuStateEnd += HideMainMenu;
-	    
-	    //subscribing the Show and Pause methods to the test events on TestButtons
-	    // TestButtons.PauseMenuStart += ShowPauseMenu;
-	    // TestButtons.PauseMenuEnd += HidePauseMenu;
-	    // TestButtons.MainMenuStart += ShowMainMenu;
-	    // TestButtons.MainMenuEnd += HideMainMenu;
-	    // TestButtons.WinScreenStart += ShowWinScreen;
-	    // TestButtons.WinScreenEnd += HideWinScreen;
-	    // TestButtons.LoseScreenStart += ShowLoseScreen;
-	    // TestButtons.LoseScreenEnd += HideLoseScreen;
+        //GameState Events MenuManager should subscribe to
+        //Hiding the Win and Lose is 
+        GameStateWon.WonStateBegin += ShowWinScreen;
+        //GameStateWon.WonStateEnd += HideWinScreen;
+        GameStateLost.LoseStateBegin += ShowLoseScreen;
+        //GameStateWon.WonStateEnd += HideLoseScreen;
+        GameStatePaused.PauseStateBegin += ShowPauseMenu;
+        GameStatePaused.PauseStateEnd += HidePauseMenu;
+        GameStateMainMenu.MainMenuStateBegin += ShowMainMenu;
+        GameStateMainMenu.MainMenuStateEnd += HideMainMenu;
+
+        //subscribing the Show and Pause methods to the test events on TestButtons
+        // TestButtons.PauseMenuStart += ShowPauseMenu;
+        // TestButtons.PauseMenuEnd += HidePauseMenu;
+        // TestButtons.MainMenuStart += ShowMainMenu;
+        // TestButtons.MainMenuEnd += HideMainMenu;
+        // TestButtons.WinScreenStart += ShowWinScreen;
+        // TestButtons.WinScreenEnd += HideWinScreen;
+        // TestButtons.LoseScreenStart += ShowLoseScreen;
+        // TestButtons.LoseScreenEnd += HideLoseScreen;
     }
-	
-	//Unsubscribe to all events on disable
-	private void OnDisable()
-	{
-		GameStateWon.WonStateBegin -= ShowWinScreen;
-		//GameStateWon.WonStateEnd -= HideWinScreen;
-		GameStateLost.LoseStateBegin -= ShowLoseScreen;
-		//GameStateWon.WonStateEnd -= HideLoseScreen;
-		GameStatePaused.PauseStateBegin -= ShowPauseMenu;
-		GameStatePaused.PauseStateEnd -= HidePauseMenu;
-		GameStateMainMenu.MainMenuStateBegin -= ShowMainMenu;
-		GameStateMainMenu.MainMenuStateEnd -= HideMainMenu;
-	} 
 
-	public void OnQuitButtonClick()
-	{
-		Application.Quit();
-	}
+    //Unsubscribe to all events on disable
+    private void OnDisable()
+    {
+        GameStateWon.WonStateBegin -= ShowWinScreen;
+        //GameStateWon.WonStateEnd -= HideWinScreen;
+        GameStateLost.LoseStateBegin -= ShowLoseScreen;
+        //GameStateWon.WonStateEnd -= HideLoseScreen;
+        GameStatePaused.PauseStateBegin -= ShowPauseMenu;
+        GameStatePaused.PauseStateEnd -= HidePauseMenu;
+        GameStateMainMenu.MainMenuStateBegin -= ShowMainMenu;
+        GameStateMainMenu.MainMenuStateEnd -= HideMainMenu;
+    }
 
-	//When the main menu is showing all the other canvas groups are hidden
-	private void ShowMainMenu()
-	{
-		Show(mainMenuGroup);
-		Hide(playingScreenGroup);
-		Hide(pauseMenuGroup);
-		Hide(winScreenGroup);
-		Hide(loseScreenGroup);
-	}
+    public void OnQuitButtonClick()
+    {
+        Application.Quit();
+    }
 
-	private void HideMainMenu()
-	{
-		Hide(mainMenuGroup);
-		Show(playingScreenGroup);
-	}
-	
-	private void ShowPauseMenu()
-	{
-		Show(pauseMenuGroup);
-		BlockInteraction(playingScreenGroup);
-	}
+    //When the main menu is showing all the other canvas groups are hidden
+    private void ShowMainMenu()
+    {
+        Show(mainMenuGroup);
+        Hide(playingScreenGroup);
+        Hide(pauseMenuGroup);
+        Hide(winScreenGroup);
+        Hide(loseScreenGroup);
+    }
 
-	private void HidePauseMenu()
-	{
-		Hide(pauseMenuGroup);
-		AllowInteraction(playingScreenGroup);
-	}
-	
-	private void ShowLoseScreen()
-	{
-		Show(loseScreenGroup);
-		Hide(playingScreenGroup);
-	}
-	
-	// private void HideLoseScreen()
-	// {
-	// 	Hide(loseScreenGroup);
-	// }
-	
-	private void ShowWinScreen()
-	{
-		Show(winScreenGroup);
-		Hide(playingScreenGroup);
-	}
-	
-	// private void HideWinScreen()
-	// {
-	// 	Hide(winScreenGroup);
-	// }
-	
-	//Hiding the screen invisible and disabling interaction
-	private void Hide(CanvasGroup group)
-	{
-		group.alpha = 0; 
-		group.interactable = false; 
-		group.blocksRaycasts = false;
-	}
+    private void HideMainMenu()
+    {
+        Hide(mainMenuGroup);
+        Show(playingScreenGroup);
+    }
 
-	private void BlockInteraction(CanvasGroup group)
-	{
-		group.interactable = false;
-		group.blocksRaycasts = false;
-	}
-	
-	private void AllowInteraction(CanvasGroup group)
-	{
-		group.interactable = true;
-		group.blocksRaycasts = true;
-	}
-	
-	//Showing the screen and enabling interaction
-	private void Show(CanvasGroup group)
-	{
-		group.alpha = 1;
-		group.interactable = true;
-		group.blocksRaycasts = true;
-	}
+    private void ShowPauseMenu()
+    {
+        Show(pauseMenuGroup);
+        BlockInteraction(playingScreenGroup);
+    }
+
+    private void HidePauseMenu()
+    {
+        Hide(pauseMenuGroup);
+        AllowInteraction(playingScreenGroup);
+    }
+
+    private void ShowLoseScreen()
+    {
+        Show(loseScreenGroup);
+        Hide(playingScreenGroup);
+    }
+
+    private void HideLoseScreen()
+    {
+        Hide(loseScreenGroup);
+    }
+
+    private void ShowWinScreen()
+    {
+        Show(winScreenGroup);
+        Hide(playingScreenGroup);
+    }
+
+    private void HideWinScreen()
+    {
+        Hide(winScreenGroup);
+    }
+
+    //Hiding the screen invisible and disabling interaction
+    private void Hide(CanvasGroup group)
+    {
+        group.alpha = 0;
+        group.interactable = false;
+        group.blocksRaycasts = false;
+    }
+
+    private void BlockInteraction(CanvasGroup group)
+    {
+        group.interactable = false;
+        group.blocksRaycasts = false;
+    }
+
+    private void AllowInteraction(CanvasGroup group)
+    {
+        group.interactable = true;
+        group.blocksRaycasts = true;
+    }
+
+    //Showing the screen and enabling interaction
+    private void Show(CanvasGroup group)
+    {
+        group.alpha = 1;
+        group.interactable = true;
+        group.blocksRaycasts = true;
+    }
 
 }
