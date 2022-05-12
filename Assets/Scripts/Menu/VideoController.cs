8d57108044e0a9dfcd67ac0b080814683d04e39a
Nathaniel Cuadros <54486315+RatFuryJunior@users.noreply.github.com>
2022-05-11 22:46:47 -0700

    Finalized Video Player, Added Black Background for video

--- Assets/Scripts/Menu/VideoController.cs
+++ Assets/Scripts/Menu/VideoController.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class VideoController : MonoBehaviour
 {
     public GameObject VideoElement;
-
+    public GameObject BlackImage;
 
     public void Awake()
     {
@@ -16,7 +17,8 @@ IEnumerator WaitForIntro()
     {
         yield return new WaitForSeconds(5.0f);
         AudioManager.Instance.Play("MenuMainNoDrums");
-        Destroy(VideoElement);        
+        Destroy(VideoElement);
+        Destroy(BlackImage);
     }
 
 }
